bbc61575dd5f53e3227c6c4b03c48af3e03cee76
SunriseChen <sunrise0918@gmail.com>
2014-11-04 12:56:11 +0800

    升级包，OData 添加 v4 的�

--- OData/App_Start/WebApiConfig.cs
+++ OData/App_Start/WebApiConfig.cs
@@ -2,6 +2,14 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web.Http;
+// OData v3
+//using Microsoft.Data.Edm;
+//using System.Web.Http.OData.Builder;
+//using System.Web.Http.OData.Extensions;
+// OData v4
+using Microsoft.OData.Edm;
+using System.Web.OData.Builder;
+using System.Web.OData.Extensions;
 
 namespace OData
 {
@@ -23,6 +31,54 @@ namespace OData
             // 若要在应用程序中禁用跟踪，请注释掉或删除以下代码行
             // 有关详细信息，请参阅: http://www.asp.net/web-api
             config.EnableSystemDiagnosticsTracing();
+
+            // OData v3:
+            // Microsoft.AspNet.WebApi.OData
+            //     Microsoft.AspNet.WebApi.Client
+            //     Microsoft.AspNet.WebApi.Core
+            //     Microsoft.Data.OData
+            //         System.Spatial
+            //         Microsoft.Data.Edm
+            // 参考：http://www.asp.net/web-api/overview/odata-support-in-aspnet-web-api/odata-v3/creating-an-odata-endpoint
+            //config.Routes.MapODataServiceRoute("ODataRoute", "odata", GetModel());
+            //config.DataServiceBehavior.MaxProtocolVersion = DataServiceProtocolVersion.V2;
+
+            // OData v4:
+            // Microsoft.AspNet.OData
+            //     Microsoft.AspNet.WebApi.Client
+            //     Microsoft.AspNet.WebApi.Core
+            //     Microsoft.OData.Core
+            //         Microsoft.Spatial
+            //         Microsoft.OData.Edm
+            // 参考：http://damienbod.wordpress.com/2014/06/10/getting-started-with-web-api-and-odata-v4/
+            // 基础教程：http://www.odata.org/getting-started/basic-tutorial/
+            // 高级教程：http://www.odata.org/getting-started/advanced-tutorial/
+            config.MapODataServiceRoute("ODataRoute", "odata", GetModel());
+
+            //config.EnsureInitialized();
+        }
+
+        public static IEdmModel GetModel()
+        {
+            var builder = new ODataConventionModelBuilder();
+
+            //var brands = builder.EntitySet<BrandDataObject>("Brands");
+            //var commodities = builder.EntitySet<CommodityDataObject>("Commodities");
+            //var banks = builder.EntitySet<BankDataObject>("Banks");
+            //var warehouses = builder.EntitySet<WarehouseDataObject>("Warehouses");
+            //var tradingCompanies = builder.EntitySet<TradingCompanyDataObject>("TradingCompanies");
+
+            //var batches = builder.EntitySet<BatchDataObject>("Batches");
+            //var contracts = builder.EntitySet<ContractDataObject>("Contracts");
+            //var batchContracts = builder.EntitySet<BatchContractDataObject>("BatchContracts");
+            //var contractDetails = builder.EntitySet<ContractDetailDataObject>("ContractDetails");
+            //var pricings = builder.EntitySet<PricingDataObject>("Pricings");
+            //var certificates = builder.EntitySet<CertificateDataObject>("Certificates");
+            //var inventories = builder.EntitySet<InventoryDataObject>("Inventories");
+            //var funds = builder.EntitySet<FundDataObject>("Funds");
+            //var invoices = builder.EntitySet<InvoiceDataObject>("Invoices");
+
+            return builder.GetEdmModel();
         }
     }
 }
