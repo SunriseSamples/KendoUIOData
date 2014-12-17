0f5642a4a168b49e4fb96d699f1518c9ca3ae562
SunriseChen <sunrise0918@gmail.com>
2014-12-17 18:09:56 +0800

    优化处理，添加本地化处理

--- KendoUI/Models/ProductViewModel.cs
+++ KendoUI/Models/ProductViewModel.cs
@@ -1,17 +1,38 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using KendoUI.Views.App_LocalResources;
 using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Web;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace KendoUI.Models
 {
+    // 参考：http://msdn.microsoft.com/zh-cn/library/system.componentmodel.dataannotations.aspx
+    [MetadataType(typeof(Metadata))]
     public class ProductViewModel
     {
-        [Key]
         public int ID { get; set; }
         public string Name { get; set; }
         public decimal Price { get; set; }
         public string Category { get; set; }
+
+        private class Metadata
+        {
+            [Key]
+            [Editable(false)]
+            [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+            [Display(ResourceType = typeof(ViewModelResource), Name = "ID_Name", ShortName = "ID_ShortName", Prompt = "ID_Prompt", Description = "ID_Description")]
+            public object ID { get; set; }
+
+            [Required]
+            [StringLength(10, MinimumLength = 3)]
+            [Display(ResourceType = typeof(ViewModelResource), Name = "Name_Name", ShortName = "Name_ShortName", Prompt = "Name_Prompt", Description = "Name_Description")]
+            public object Name { get; set; }
+
+            [Display(ResourceType = typeof(ViewModelResource), Name = "Price_Name", ShortName = "Price_ShortName", Prompt = "Price_Prompt", Description = "Price_Description")]
+            public object Price { get; set; }
+
+            [StringLength(150)]
+            [ForeignKey("Category")]
+            [Display(ResourceType = typeof(ViewModelResource), Name = "Category_Name", ShortName = "Category_ShortName", Prompt = "Category_Prompt", Description = "Category_Description")]
+            public object Category { get; set; }
+        }
     }
 }
